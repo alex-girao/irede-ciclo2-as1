bc8e1e476164a4cffc57609f4e8ccf45caf46ce1
Alex Girão <fco.alex.girao@gmail.com>
2026-09-01 21:41:36 -0300

    Adicionando eventos

--- unidade03/capitulo03/projetos/cadastrojavafx/src/main/java/br/org/irede/grafico/cadastrojavafx/PrimaryController.java
+++ unidade03/capitulo03/projetos/cadastrojavafx/src/main/java/br/org/irede/grafico/cadastrojavafx/PrimaryController.java
@@ -1,9 +1,26 @@
 package br.org.irede.grafico.cadastrojavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * Classe evocada para administrar eventos da tela primary.fxml
  * @author macmini
  */
 public class PrimaryController {
+    
+    @FXML
+    private TextField txtNome;
+    
+    @FXML
+    private void adicionarNome(){
+        String nome = txtNome.getText().trim();
+        System.out.println(">>>> " + nome);
+    }
+    
+    @FXML
+    private void limparCampos(){
+        txtNome.clear();
+    }
 
 }
