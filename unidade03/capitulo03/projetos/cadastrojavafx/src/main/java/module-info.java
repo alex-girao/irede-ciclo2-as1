0a8529d36af5f7dea3eb18fdd0e8ee82f1c37615
Alex Girão <fco.alex.girao@gmail.com>
2026-09-03 20:34:28 -0300

    Finalização de CRUD

--- unidade03/capitulo03/projetos/cadastrojavafx/src/main/java/module-info.java
+++ unidade03/capitulo03/projetos/cadastrojavafx/src/main/java/module-info.java
@@ -3,5 +3,7 @@ module br.org.irede.grafico.cadastrojavafx {
     requires javafx.fxml;
 
     opens br.org.irede.grafico.cadastrojavafx to javafx.fxml;
+    opens br.org.irede.grafico.cadastrojavafx.model to javafx.base;
     exports br.org.irede.grafico.cadastrojavafx;
+    
 }
